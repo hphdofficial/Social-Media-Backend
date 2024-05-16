e12ba76465318fee4ab32e270c7fb7b70a7fab4a
hphdofficial <hphdofficial@gmail.com>
2024-05-17 05:57:48 +0700

    Re-upload project

---
+++ src/main/java/com/socialmedia/repository/NotificationRepository.java
@@ -0,0 +1,14 @@
+package com.socialmedia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.socialmedia.model.Notifications;
+import com.socialmedia.model.Users;
+
+public interface NotificationRepository extends JpaRepository<Notifications, Integer> {
+
+    public List<Notifications> findByUserOrderByIdDesc(Users user);
+    
+}
